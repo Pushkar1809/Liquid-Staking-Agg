fe607a9c13825bba6632ae21a4b94e4710668407
Pushkar Borkar <pushkarborkar1809@gmail.com>
2023-12-18 17:23:03 +0530

    Add tooltip for minified icons

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -1,9 +1,10 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import { UserContext } from "../context/UserContext";
 import { useUser } from "../hooks/useUser";
 import { styled } from 'styled-components';
 import { IoPerson } from "react-icons/io5";
 import { FaUnlink } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
 import { WinDimContext } from "../context/WinDimContext";
 
 const AccountTag = styled.div`
@@ -37,7 +38,20 @@ const Button = styled.button`
 	}
 `;
 
+const ModalCard = styled.div`
+	padding: 1rem;
+	background: white;
+	border-radius: 0.5rem;
+	border: 1px solid var(--color-accent);
+	box-shadow: 2px 2px 12px rgba(0,0,0,0.15);
+	position: absolute;
+	right:0;
+	bottom:-3.5rem;
+`
+
 const ConnectWallet = () => {
+	const [isAddressVisible, setIsAddressVisible] = useState(false);
+	const [isNetworkErrorVisible, setIsNetworkErrorVisible] = useState(false);
     const { address, isCorrectNetwork } = useContext(UserContext);
     const { width } = useContext(WinDimContext);
     const { initialise } = useUser();
@@ -45,13 +59,53 @@ const ConnectWallet = () => {
     return (
 			<div>
 				{!!address ? (
-					<div style={{ display: "flex", alignItems: "center" }}>
+					<div
+						style={{
+							position: "relative",
+							display: "flex",
+							alignItems: "center",
+						}}>
 						{!isCorrectNetwork && (
-							<Button style={{ background: "red", marginRight: "0.75rem" }}>
-								{width > 768 ? "Switch to Goerli" : <FaUnlink />}
-							</Button>
+							<>
+								<Button
+									onClick={() => {
+										setIsNetworkErrorVisible(!isNetworkErrorVisible);
+										if(isAddressVisible) {setIsAddressVisible(false);}
+									}}
+									style={{ background: "red", marginRight: "0.75rem" }}>
+									{width > 768 ? (
+										"Switch to Goerli"
+									) : isNetworkErrorVisible ? (
+										<RxCross2 />
+									) : (
+										<FaUnlink />
+									)}
+								</Button>
+								{width <= 768 && isNetworkErrorVisible && (
+									<ModalCard style={{ color: "red", borderColor: "red", minWidth: "17ch" }}>
+										Switch to Goerli
+									</ModalCard>
+								)}
+							</>
 						)}
-						<AccountTag>{width > 768 ? address : <IoPerson />}</AccountTag>
+						<AccountTag
+							onClick={() => {
+								setIsAddressVisible(!isAddressVisible)
+								if (isNetworkErrorVisible) {
+									setIsNetworkErrorVisible(false);
+								}
+							}}>
+							{width > 768 ? (
+								address
+							) : isAddressVisible ? (
+								<RxCross2 />
+							) : (
+								<IoPerson />
+							)}
+						</AccountTag>
+						{width <= 768 && isAddressVisible && (
+							<ModalCard>{address}</ModalCard>
+						)}
 					</div>
 				) : (
 					<Button onClick={initialise}>Connect Wallet</Button>
