ee5ca776c5295f76edf28580dcc9962411f3e459
Pushkar Borkar <pushkarborkar1809@gmail.com>
2023-12-18 07:38:51 +0530

    Add Stake Unstake Tabs

--- src/components/StakeCard.jsx
+++ src/components/StakeCard.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 import ProviderSelect from './ProviderSelect';
 import AmountInput from "./AmountInput";
+import StakeUnstakeTabs from "./StakeUnstakeTabs";
 
 const Card = styled.div`
-    padding: 1rem;
+    padding: 2rem;
     border-radius: 1rem;
-    border: 1px solid black;
     width: 50%;
     margin: 0 auto;
+    background-color: white;
+    position: relative;
+    margin-top: 5rem;
 `
 
 const StakeCard = () => {
+    const [activeTask, setActiveTask] = useState('stake')
     return(
         <Card>
+            {/* 0. Header to select the job Stake/Unstake */}
+            <StakeUnstakeTabs activeTask={activeTask} setActiveTask={setActiveTask}/>
+
             {/* 1. Liquidity Provider to choose
              Tabs for Strader and Lido */}
             <ProviderSelect/>
@@ -33,8 +40,8 @@ const StakeCard = () => {
             </div>
 
             {/* 4. Stake/Unstake button */}
-            <button>Stake</button>
-
+            {activeTask==='stake' ? <button>Stake</button> : <button>Unstake</button>}
+            
             {/* 5. Optional Info cards */}
         </Card>
     );
---
+++ src/components/StakeUnstakeTabs.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Tabs = styled.div`
+	position: absolute;
+    top: -2.75rem;
+    left: 0;
+    display: flex;
+    align-items: center;
+`;
+
+const Tab = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	cursor: pointer;
+    padding: 1rem 2rem;
+    border-radius: 1rem 1rem 0 0;
+    background-color: white;
+`;
+
+const StakeUnstakeTabs = ({activeTask, setActiveTask}) => {
+    return (
+			<Tabs>
+				<Tab onClick={()=> setActiveTask('stake')} style={{ opacity: activeTask === "stake" ? 1 : 0.5 }}>Stake</Tab>
+				<Tab onClick={() => setActiveTask('unstake')} style={{ opacity: activeTask === "unstake" ? 1 : 0.5 }}>
+					Unstake
+				</Tab>
+			</Tabs>
+		);
+};
+
+export default StakeUnstakeTabs;
