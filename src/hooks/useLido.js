a7a121537f28cc962e14c21aabf2766910ea2e55
Restyled.io <commits@restyled.io>
2024-05-30 07:41:47 +0000

    Restyled by clang-format

--- src/hooks/useLido.js
+++ src/hooks/useLido.js
@@ -1,84 +1,96 @@
-import { Contract, ZeroAddress, parseEther, formatUnits } from 'ethers';
+import {Contract, formatUnits, parseEther, ZeroAddress} from 'ethers';
+import {useContext, useEffect, useState} from 'react';
+
 import LidoABI from "../abis/LidoABI.json";
 import WithdrawlQueueABI from "../abis/WithdrawlQueueABI.json";
-import { useContext, useState, useEffect } from 'react';
-import { UserContext } from '../context/UserContext';
+import {UserContext} from '../context/UserContext';
 
 export const useLido = () => {
-    const { signer, address, setIsStETHApproved, setStETHBalance } = useContext(UserContext);
-    const [contracts, setContracts] = useState({
-        lido: null,
-        withdrawlQueue: null
-    })
+  const {signer, address, setIsStETHApproved, setStETHBalance} =
+      useContext(UserContext);
+  const [contracts, setContracts] =
+      useState({lido : null, withdrawlQueue : null})
 
-    const lidoAddress = "0x1643E812aE58766192Cf7D2Cf9567dF2C37e9B7F";
-	const withdrawlQueueAddress = "0xCF117961421cA9e546cD7f50bC73abCdB3039533";
+  const lidoAddress = "0x1643E812aE58766192Cf7D2Cf9567dF2C37e9B7F";
+  const withdrawlQueueAddress = "0xCF117961421cA9e546cD7f50bC73abCdB3039533";
 
-    useEffect(() => {
-        const lido = new Contract(lidoAddress, LidoABI, signer);
-        const withdrawlQueue = new Contract(
-					withdrawlQueueAddress,
-					WithdrawlQueueABI,
-					signer,
-				);
-        setContracts({ lido, withdrawlQueue });
-		}, [signer]);
+  useEffect(() => {
+    const lido = new Contract(lidoAddress, LidoABI, signer);
+    const withdrawlQueue = new Contract(
+        withdrawlQueueAddress,
+        WithdrawlQueueABI,
+        signer,
+    );
+    setContracts({lido, withdrawlQueue});
+  }, [ signer ]);
 
-    const stake = async (value) => {
-        try {
-            await contracts.lido.submit(ZeroAddress, {value});
-        } catch (err) {
-            console.error("Failed to Stake", err);
-        }
+  const stake =
+      async (value) => {
+    try {
+      await contracts.lido.submit(ZeroAddress, {value});
+    } catch (err) {
+      console.error("Failed to Stake", err);
     }
+  }
 
-    const unstake = async (value) => {
-        console.log(value);
-        try {
-            await contracts.withdrawlQueue.requestWithdrawals([value], address); 
-        } catch (err) {
-            console.error("Failed to Unstake", err);
-        }
+  const unstake =
+      async (value) => {
+    console.log(value);
+    try {
+      await contracts.withdrawlQueue.requestWithdrawals([ value ], address);
+    } catch (err) {
+      console.error("Failed to Unstake", err);
     }
+  }
 
-    const getBalance = async () => {
-        try {
-            const balance = await contracts.lido.balanceOf(address);
-            setStETHBalance(formatUnits(balance, 18));
-        } catch (err) {
-            console.error("Failed to get balance", err);
-        }
+  const getBalance =
+      async () => {
+    try {
+      const balance = await contracts.lido.balanceOf(address);
+      setStETHBalance(formatUnits(balance, 18));
+    } catch (err) {
+      console.error("Failed to get balance", err);
     }
+  }
 
-    const approve = async (amount=10) => {
-        try {
-            await contracts.lido.approve(withdrawlQueueAddress, parseEther(amount));
-        } catch (err) {
-            console.log("Failed to Approve stETH", err);
-        }
+  const approve =
+      async (amount = 10) => {
+    try {
+      await contracts.lido.approve(withdrawlQueueAddress, parseEther(amount));
+    } catch (err) {
+      console.log("Failed to Approve stETH", err);
     }
+  }
 
-    const isApproved = async () => {
-        try {
-            const allowance = await contracts.lido.allowance(
-                address,
-                withdrawlQueueAddress,
-            );
-            setIsStETHApproved(allowance > parseEther("0"))
-        } catch (err) {
-            console.log("Failed to check approval status", err);
-        }
-    };
-
-    const getExchangeRate = async () => {
-        try {
-            const rate = await contracts.lido.getPooledEthByShares(1);
-            return rate;
-        } catch (err) {
-            console.error("Failed to get exhange rate", err);
-        }
+  const isApproved = async () => {
+    try {
+      const allowance = await contracts.lido.allowance(
+          address,
+          withdrawlQueueAddress,
+      );
+      setIsStETHApproved(allowance > parseEther("0"))
+    } catch (err) {
+      console.log("Failed to check approval status", err);
     }
+  };
 
-    return {contracts, stake, unstake, getBalance, approve, isApproved, getExchangeRate};
+  const getExchangeRate =
+      async () => {
+    try {
+      const rate = await contracts.lido.getPooledEthByShares(1);
+      return rate;
+    } catch (err) {
+      console.error("Failed to get exhange rate", err);
+    }
+  }
+
+  return {
+    contracts,
+    stake,
+    unstake,
+    getBalance,
+    approve,
+    isApproved,
+    getExchangeRate
+  };
 }
-
--- src/hooks/useStader.js
+++ src/hooks/useStader.js
@@ -1,94 +1,105 @@
-import {Contract, parseEther, formatUnits} from 'ethers';
-import { useContext, useState, useEffect } from 'react';
-import { UserContext } from '../context/UserContext';
-import StaderPoolManagerABI from "../abis/StaderPoolManagerABI.json";
+import {Contract, formatUnits, parseEther} from 'ethers';
+import {useContext, useEffect, useState} from 'react';
+
 import ETHxABI from "../abis/ETHxABI.json";
+import StaderPoolManagerABI from "../abis/StaderPoolManagerABI.json";
 import userWithdrawlManagerABI from "../abis/UserWithdrawlManagerABI.json"
+import {UserContext} from '../context/UserContext';
 
 export const useStader = () => {
-    const { signer, address, setIsETHxApproved, setETHxBalance } = useContext(UserContext);
-    const [contracts, setContracts] = useState({
-        staderPoolManager: null,
-        ETHx: null,
-        userWithdrawlManager: null
-    });
+  const {signer, address, setIsETHxApproved, setETHxBalance} =
+      useContext(UserContext);
+  const [contracts, setContracts] = useState(
+      {staderPoolManager : null, ETHx : null, userWithdrawlManager : null});
 
-    const staderPoolManagerAdress =
-			"0x7F09ceb3874F5E35Cd2135F56fd4329b88c5d119";
-    const ETHxAddress = "0xB4F5fc289a778B80392b86fa70A7111E5bE0F859";
-    const userWithdrawlManagerAddress =
-			"0x3F6F1C1081744c18Bd67DD518F363B9d4c76E1d2";
+  const staderPoolManagerAdress = "0x7F09ceb3874F5E35Cd2135F56fd4329b88c5d119";
+  const ETHxAddress = "0xB4F5fc289a778B80392b86fa70A7111E5bE0F859";
+  const userWithdrawlManagerAddress =
+      "0x3F6F1C1081744c18Bd67DD518F363B9d4c76E1d2";
 
-    useEffect(() => {
-        const staderPoolManager = new Contract(
-            staderPoolManagerAdress,
-            StaderPoolManagerABI,
-            signer,
-        );
-        const ETHx = new Contract(ETHxAddress, ETHxABI, signer);
-        const userWithdrawlManager = new Contract(
-            userWithdrawlManagerAddress,
-            userWithdrawlManagerABI,
-            signer,
-        );
-        setContracts({ staderPoolManager, ETHx, userWithdrawlManager });
-    }, [signer]);
+  useEffect(() => {
+    const staderPoolManager = new Contract(
+        staderPoolManagerAdress,
+        StaderPoolManagerABI,
+        signer,
+    );
+    const ETHx = new Contract(ETHxAddress, ETHxABI, signer);
+    const userWithdrawlManager = new Contract(
+        userWithdrawlManagerAddress,
+        userWithdrawlManagerABI,
+        signer,
+    );
+    setContracts({staderPoolManager, ETHx, userWithdrawlManager});
+  }, [ signer ]);
 
-    const stake = async (value) => {
-        try {
-            await contracts.staderPoolManager.deposit(address, 1, {value});
-        } catch (err) {
-            console.error("Failed to stake", err)
-        }
+  const stake =
+      async (value) => {
+    try {
+      await contracts.staderPoolManager.deposit(address, 1, {value});
+    } catch (err) {
+      console.error("Failed to stake", err)
     }
+  }
 
-    const unstake = async (value) => {
-        console.log(value)
-        try {
-            await contracts.userWithdrawlManager.requestWithdraw(value, address);
-        } catch (err) {
-            console.error("Unstaking Failed", err);
-        }
-        
+  const unstake =
+      async (value) => {
+    console.log(value)
+    try {
+      await contracts.userWithdrawlManager.requestWithdraw(value, address);
+    } catch (err) {
+      console.error("Unstaking Failed", err);
     }
+  }
 
-    const getExchangeRate = async () => {
-        try {
-            const rate = await contracts.staderPoolManager.getExchangeRate();
-            return formatUnits(rate, 18);
-        } catch (err) {
-            console.error("Getting Exchange Rate unscessesful", err);
-        }
+  const getExchangeRate =
+      async () => {
+    try {
+      const rate = await contracts.staderPoolManager.getExchangeRate();
+      return formatUnits(rate, 18);
+    } catch (err) {
+      console.error("Getting Exchange Rate unscessesful", err);
     }
+  }
 
-    const getBalance = async () => {
-        try {
-            const balance = await contracts.ETHx.balanceOf(address);
-            setETHxBalance(formatUnits(balance, 18));
-        } catch (err) {
-            console.error("Failed to get balance", err);
-        }
-    };
-
-    const approve = async (amount=10) => {
-        try {
-            await contracts.ETHx.approve(userWithdrawlManagerAddress, parseEther(amount));
-        } catch (err) {
-            console.error("Failed to Approve ETHx", err)
-        }
+  const getBalance = async () => {
+    try {
+      const balance = await contracts.ETHx.balanceOf(address);
+      setETHxBalance(formatUnits(balance, 18));
+    } catch (err) {
+      console.error("Failed to get balance", err);
     }
+  };
 
-    const isApproved = async () => {
-        try {
-            const allowance = await contracts.ETHx.allowance(
-							address,
-							userWithdrawlManagerAddress,
-						);
-            setIsETHxApproved(allowance > parseEther("0"));
-        } catch (err) {
-            console.error("Failed to check approval status", err);
-        }
+  const approve =
+      async (amount = 10) => {
+    try {
+      await contracts.ETHx.approve(userWithdrawlManagerAddress,
+                                   parseEther(amount));
+    } catch (err) {
+      console.error("Failed to Approve ETHx", err)
     }
+  }
 
-    return {contracts, stake, unstake, getExchangeRate, getBalance, approve, isApproved};
+  const isApproved =
+      async () => {
+    try {
+      const allowance = await contracts.ETHx.allowance(
+          address,
+          userWithdrawlManagerAddress,
+      );
+      setIsETHxApproved(allowance > parseEther("0"));
+    } catch (err) {
+      console.error("Failed to check approval status", err);
+    }
+  }
+
+  return {
+    contracts,
+    stake,
+    unstake,
+    getExchangeRate,
+    getBalance,
+    approve,
+    isApproved
+  };
 }
